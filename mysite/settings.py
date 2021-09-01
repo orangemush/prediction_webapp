c8bb70f920dc66e20ee86312da46242a6fbb9bcd
TaeKi Moon <orangemush99@gmail.com>
2021-09-02 00:54:55 +0900

    movie_number_of_audience_prediction update

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = my_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "orangemush.pythonanywhere.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
